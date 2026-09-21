ef7b9880704d39d1c0b779c0aa0dec6a34bb8425
Juan <jd.ramirez@unitecnica.net>
2026-09-21 09:52:08 -0500

    Se agregó la variable numero3

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 numero1 = float(input("Ingrese el primer número: "))
 numero2 = float(input("Ingrese el segundo número: "))
-
+numero2 = float(input("Ingrese el tercer número: "))
 resultado = numero1 + numero2
 
 print("Resultado:", resultado)
