b9b5b3e8c23e35864dbc78ddbbf30b72da1d708f
Juan <jd.ramirez@unitecnica.net>
2026-09-21 09:56:29 -0500

    Se eliminó la variable numero3

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 numero1 = float(input("Ingrese el primer número: "))
 numero2 = float(input("Ingrese el segundo número: "))
-numero3 = float(input("Ingrese el tercer número: "))
 resultado = numero1 + numero2 
 
 print("Resultado:", resultado)
